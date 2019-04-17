4c4fabbd27ed363610f80c0008bf951b9dc03fac
manishkumr <rishimanish123@gmail.com>
2019-04-18 09:57:08 +1000

    Adding functional Todo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function Todo({todo, index}) {
   return (
-      <div>Hello</div>
+      <div className="todo">
+        {todo.text}
+      </div>
+  )
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false
+    },
+    {
+      text: "Build really cool todo app",
+      isCompleted: false
+    }
+  ]);
+  return (
+      <div className="app">
+        <div className="todo-list">
+          {todos.map((todo, index) => (
+              <Todo
+                  key = {index}
+                  index = {index}
+                  todo= {todo}
+              />
+          ))}
+        </div>
+      </div>
   )
 }
 
